2dc8992f7533fce6bada7aaea81cd16e9d6fae57
Abishek R Srikaanth <abishek@technicaliti.me>
2013-10-25 01:09:48 +0530

    Fixed psr-0 fixes by running php-cs-fixer

--- src/aftership/Tracking.php
+++ src/aftership/Tracking.php
@@ -1,46 +1,51 @@
 <?php
 
-namespace AfterShip;
+namespace aftership;
 
 use AfterShip\core\request;
 
 class Tracking extends request
 {
-	public function __construct($api_key) {
-		if (empty($api_key))
-			throw new \Exception('API Key is missing');
-		$this->_api_key = $api_key;
+    public function __construct($api_key)
+    {
+        if (empty($api_key))
+            throw new \Exception('API Key is missing');
+        $this->_api_key = $api_key;
 
-	}
+    }
 
-	public function create(array $data) {
-		if (empty($data))
-			throw new \Exception('Tracking Request cannot be empty');
+    public function create(array $data)
+    {
+        if (empty($data))
+            throw new \Exception('Tracking Request cannot be empty');
 
-		return $this->send('trackings', 'POST', json_encode(array('tracking' => $data)));
-	}
+        return $this->send('trackings', 'POST', json_encode(array('tracking' => $data)));
+    }
 
-	public function get(array $options = array()) {
-		return $this->send('trackings', 'GET', $options);
-	}
+    public function get(array $options = array())
+    {
+        return $this->send('trackings', 'GET', $options);
+    }
 
-	public function info($slug, $tracking_number, array $fields = array()) {
-		if (empty($slug))
-			throw new \Exception("Slug cannot be empty");
+    public function info($slug, $tracking_number, array $fields = array())
+    {
+        if (empty($slug))
+            throw new \Exception("Slug cannot be empty");
 
-		if (empty($tracking_number))
-			throw new \Exception('Tracking number cannot be empty');
+        if (empty($tracking_number))
+            throw new \Exception('Tracking number cannot be empty');
 
-		return $this->send('trackings/' . $slug . '/' . $tracking_number, 'GET', $fields);
-	}
+        return $this->send('trackings/' . $slug . '/' . $tracking_number, 'GET', $fields);
+    }
 
-	public function update($slug, $tracking_number, array $options) {
-		if (empty($slug))
-			throw new \Exception("Slug cannot be empty");
+    public function update($slug, $tracking_number, array $options)
+    {
+        if (empty($slug))
+            throw new \Exception("Slug cannot be empty");
 
-		if (empty($tracking_number))
-			throw new \Exception('Tracking number cannot be empty');
+        if (empty($tracking_number))
+            throw new \Exception('Tracking number cannot be empty');
 
-		return $this->send('trackings/' . $slug . '/' . $tracking_number, 'PUT', json_encode(array('tracking' => $options)));
-	}
+        return $this->send('trackings/' . $slug . '/' . $tracking_number, 'PUT', json_encode(array('tracking' => $options)));
+    }
 }
--- src/aftership/core/request.php
+++ src/aftership/core/request.php
@@ -1,33 +1,34 @@
 <?php
-namespace AfterShip\core;
+namespace aftership\core;
 
 use Guzzle\Http\Client;
 
 class request
 {
-	private $_api_url = 'https://api.aftership.com';
-	protected $_api_key = '';
-	private $_api_version = 'v3';
+    private $_api_url = 'https://api.aftership.com';
+    protected $_api_key = '';
+    private $_api_version = 'v3';
 
-	protected function send($url, $request_type, $data = array()) {
-		$client  = new Client();
-		$headers = array(
-			'aftership-api-key' => $this->_api_key
-		);
-		switch (strtoupper($request_type)) {
-			case "GET":
-				$request = $client->get($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, array('query' => $data));
-				break;
-			case "POST":
-				$request = $client->post($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, $data);
-				break;
-			case "PUT":
-				$request = $client->put($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, $data);
-				break;
-		}
+    protected function send($url, $request_type, $data = array())
+    {
+        $client  = new Client();
+        $headers = array(
+            'aftership-api-key' => $this->_api_key
+        );
+        switch (strtoupper($request_type)) {
+            case "GET":
+                $request = $client->get($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, array('query' => $data));
+                break;
+            case "POST":
+                $request = $client->post($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, $data);
+                break;
+            case "PUT":
+                $request = $client->put($this->_api_url . '/' . $this->_api_version . '/' . $url, $headers, $data);
+                break;
+        }
 
-		$response = $request->send()->json();
+        $response = $request->send()->json();
 
-		return $response;
-	}
+        return $response;
+    }
 }

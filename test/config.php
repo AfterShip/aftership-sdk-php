f623fc350131ccd597b8eb3220102868d4cf0a67
Sunny Chow <sunny.itp@gmail.com>
2014-08-05 10:32:40 +0800

    Testing

--- test/config.php
+++ test/config.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * User: Sunny Chow
- * Date: 4/8/14
- * Time: 6:19 PM
- */
-
 
 $api_key = '';
+$request_all = false;
--- test/testing.php
+++ test/testing.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * User: Sunny Chow
- * Date: 4/8/14
- * Time: 1:05 PM
- */
+
 ?>
 
 <html>
@@ -27,13 +23,15 @@ include_once('config.php');
 include_once('../vendor/autoload.php');
 
 $action = isset($_GET['action']) ? $_GET['action'] : '';
-$debug = true;
 
 global $api_key;
 echo 'API KEY: ' . $api_key;
 print '</br>';
 echo 'ACTION: '. $action;
 print '</br>';
+echo 'Request All: '. $request_all;
+print '</br>';
+
 
 if (isset($api_key)){
 	echo '</br>';
@@ -59,17 +57,17 @@ echo '<input type="hidden" name="action" id="hidden"/>';
 $couriers = new AfterShip\Couriers($api_key);
 echo '<h1>Couriers</h1>';
 echo '<input type="button" value="couriers_get" class="btn">' . 'get user\'s couriers' . '</br>';
-if ($debug || $action == 'couriers_get') {
+if ($request_all || $action == 'couriers_get') {
 	p($couriers->get());
 }
 
 echo '<input type="button" value="couriers_get_all" class="btn">' . 'get all couriers' . '</br>';
-if ($debug || $action == 'couriers_get_all') {
+if ($request_all || $action == 'couriers_get_all') {
 	p($couriers->get_all());
 }
 
 echo '<input type="button" value="couriers_detect" class="btn">' . 'detect courier by tracking number' . '</br>';
-if ($debug || $action == 'couriers_detect') {
+if ($request_all || $action == 'couriers_detect') {
 	p($couriers->detect('1ZV90R483A33906706'));
 }
 
@@ -77,29 +75,29 @@ if ($debug || $action == 'couriers_detect') {
 $trackings = new AfterShip\Trackings($api_key);
 echo '<h1>Trackings</h1>';
 echo '<input type="button" value="trackings_create" class="btn">' . 'create tracking' . '</br>';
-if ($debug || $action == 'trackings_create') {
+if ($request_all || $action == 'trackings_create') {
 	p($trackings->create('2254095771'));
 }
 
 /*
 echo '<input type="button" value="couriers_get" class="btn">'.'batch create'.'</br>';
-if ($debug || $action == 'couriers_get'){
+if ($request_all || $action == 'couriers_get'){
 p($trackings->batch_create(array('1ZV90R483A33906706')));
 }
 */
 
 echo '<input type="button" value="trackings_delete" class="btn">' . 'delete tracking' . '</br>';
-if ($debug || $action == 'trackings_delete') {
+if ($request_all || $action == 'trackings_delete') {
 	p($trackings->delete('ups', '1ZV90R483A33906706'));
 }
 
 echo '<input type="button" value="trackings_delete_by_id" class="btn">' . 'delete tracking by id' . '</br>';
-if ($debug || $action == 'trackings_delete_by_id') {
+if ($request_all || $action == 'trackings_delete_by_id') {
 	p($trackings->delete_by_id('53df4d66868a6df243b6f882'));
 }
 
 echo '<input type="button" value="trackings_get_all" class="btn">' . 'get all trackings' . '</br>';
-if ($debug || $action == 'trackings_get_all') {
+if ($request_all || $action == 'trackings_get_all') {
 	p($trackings->get_all(array(
 		'slug' => 'dhl',
 		'fields' => 'title,order_id,message,country_name'
@@ -107,12 +105,12 @@ if ($debug || $action == 'trackings_get_all') {
 }
 
 echo '<input type="button" value="trackings_get" class="btn">' . 'get a tracking' . '</br>';
-if ($debug || $action == 'trackings_get') {
+if ($request_all || $action == 'trackings_get') {
 	p($trackings->get('dhl', '2254095771'));
 }
 
 echo '<input type="button" value="trackings_get_by_id" class="btn">' . 'get a tracking by id' . '</br>';
-if ($debug || $action == 'trackings_get_by_id') {
+if ($request_all || $action == 'trackings_get_by_id') {
 	p($trackings->get_by_id('53df4a90868a6df243b6efd8', array(
 		'fields' => 'customer_name'
 	)));
@@ -120,14 +118,14 @@ if ($debug || $action == 'trackings_get_by_id') {
 
 
 echo '<input type="button" value="trackings_update" class="btn">' . 'update a tracking' . '</br>';
-if ($debug || $action == 'trackings_update') {
+if ($request_all || $action == 'trackings_update') {
 	p($trackings->update('ups', '1ZV90R483A33906706', array(
 		'title' => 'haha'
 	)));
 }
 
 echo '<input type="button" value="trackings_update_by_id" class="btn">' . 'update a tracking by id' . '</br>';
-if ($debug || $action == 'trackings_update_by_id') {
+if ($request_all || $action == 'trackings_update_by_id') {
 	p($trackings->update_by_id('53df4a90868a6df243b6efd8'), array(
 		'title' => 'T1',
 		'customer_name' => 'Sunny'
@@ -136,12 +134,12 @@ if ($debug || $action == 'trackings_update_by_id') {
 
 
 echo '<input type="button" value="trackings_retrack" class="btn">' . 'retrack a tracking' . '</br>';
-if ($debug || $action == 'trackings_retrack') {
+if ($request_all || $action == 'trackings_retrack') {
 	p($trackings->retrack('dhl', '2254095771'));
 }
 
 echo '<input type="button" value="trackings_retrack_by_id" class="btn">' . 'retrack a tracking by id' . '</br>';
-if ($debug || $action == 'trackings_retrack_by_id') {
+if ($request_all || $action == 'trackings_retrack_by_id') {
 	p($trackings->retrack_by_id('53df4a90868a6df243b6efd8'));
 }
 
@@ -149,12 +147,12 @@ if ($debug || $action == 'trackings_retrack_by_id') {
 $last_check_point = new AfterShip\LastCheckPoint($api_key);
 echo '<h1>Last Check Point</h1>';
 echo '<input type="button" value="last_check_point_get" class="btn">' . 'get' . '</br>';
-if ($debug || $action == 'last_check_point_get') {
+if ($request_all || $action == 'last_check_point_get') {
 	p($last_check_point->get('dhl', '2254095771'));
 }
 
 echo '<input type="button" value="last_check_point_get_by_id" class="btn">' . 'get by id' . '</br>';
-if ($debug || $action == 'last_check_point_get_by_id') {
+if ($request_all || $action == 'last_check_point_get_by_id') {
 	p($last_check_point->get_by_id('53df4a90868a6df243b6efd8', array(
 		'fields' => 'city,zip,state'
 	)));
